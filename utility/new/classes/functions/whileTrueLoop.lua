b140341b916cbe85310869c12983b01dd4bb0ca8
0aoq <73413330+0aoq@users.noreply.github.com>
2021-07-30 21:49:10 -0400

    Create whileTrueLoop.lua

---
+++ utility/new/classes/functions/whileTrueLoop.lua
@@ -0,0 +1,31 @@
+-- whileTrueLoop
+-- author: @roblox/0a_oq, @github/0aoq
+
+return {
+	name = script.Name,
+	constructor = function(delay, luauFunction)
+		local allowed = true
+
+		coroutine.resume(coroutine.create(function()
+			while true do
+				wait(delay)
+				
+				if (allowed) then 
+					luauFunction() 
+				else
+					break; end
+			end
+		end))
+		
+		-- object functions
+
+		local properties = {} do
+			function properties:Destroy() 
+				wait(delay)
+				allowed = false
+			end
+		end
+
+		return properties
+	end,
+}
