b9fbf4205dbbfe78ccfd64d3bbc67664580a3ca0
0aoq <73413330+0aoq@users.noreply.github.com>
2021-07-31 09:57:57 -0400

    Create HumanoidAnimation.lua

---
+++ utility/new/classes/functions/HumanoidAnimation.lua
@@ -0,0 +1,31 @@
+-- HumanoidAnimation
+-- author: @roblox/0a_oq, @github/0aoq
+
+local Players = game:GetService("Players")
+
+return {
+	name = script.Name,
+	constructor = function(humanoid: Humanoid, anim: string)
+		local track
+		if (anim ~= nil) then
+			local animator = humanoid:FindFirstChild("Animator")
+			track = Instance.new("Animation")
+			track.Name = "GunAnim"
+			track.AnimationId = anim
+			track.Parent = animator
+			track = animator:LoadAnimation(track)
+			track:Play()
+			track:Stop()
+		end
+
+		-- object functions
+
+		local properties = {} do
+			function properties:Play()
+				track:Play(); end
+			
+			function properties:Stop()
+				track:Stop(); end
+		end; return properties, track
+	end,
+}
