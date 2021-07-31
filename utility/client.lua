32d8e207023dfb726da817ea1f45f7f91777ca61
0aoq <73413330+0aoq@users.noreply.github.com>
2021-07-30 21:59:50 -0400

    Create client.lua

---
+++ utility/client.lua
@@ -0,0 +1,37 @@
+--[[
+
+# RBLX SCRIPT UTLITY
+
+// BEGIN JSON
+
+{
+	"Author Information": {
+		"@roblox": "0a_oq",
+		"@github": "0aoq"
+	},
+		
+	"Project Information": {
+		"name": "rblxScriptUtility",
+		"description": "Basic utility modules for roblox games."
+		"repository": "@github/0aoq/rblxScriptUtility",
+	}
+}
+
+// END JSON
+
+This module can be required so that you don't have to individually require each module by yourself.
+
+/// BEGIN SNIPPET
+local utility = require(script.Parent.utility.client);
+	local require = utility.require;
+	local new = utility.new
+/// END SNIPPET
+
+Keep in mind that this is just a small WIP project, and my be broken/missing features!!!
+
+]]
+
+local export = {} do
+	export.require = require(script.Parent.loader)
+	export.new = export.require({ "*" }, script.Parent.new.main)
+end; return export
