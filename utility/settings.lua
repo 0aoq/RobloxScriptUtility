7ebaecb4285ba0c784b9efb9a318596e8e241cba
0aoq <73413330+0aoq@users.noreply.github.com>
2021-08-01 12:36:14 -0400

    Create settings.lua

---
+++ utility/settings.lua
@@ -0,0 +1,18 @@
+--[[
+====================================================================
+Licensed under the MIT license
+--------------------------------------------------------------------
+More information on the license at 
+https://github.com/0aoq/RobloxScriptUtility/blob/main/LICENSE
+====================================================================
+]]
+
+-- information at https://github.com/0aoq/RobloxScriptUtility/wiki/Settings
+
+return {
+	-- // debug
+	shoutClasses = true,
+	-- // features
+	service = true,
+	addEventListener = true
+}
